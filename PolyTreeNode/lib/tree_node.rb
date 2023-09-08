ca6eaea7b553ff65cd7b49c661f93987f8a7ed8a
Allon Nam <allon.nam96@gmail.com>
2023-09-08 15:43:22 -0400

    dfs

--- PolyTreeNode/lib/tree_node.rb
+++ PolyTreeNode/lib/tree_node.rb
@@ -1,44 +1,45 @@
 class PolyTreeNode
 
-attr_reader :value, :parent, :children
-    def initialize(value)
-        @value = value
-        @parent = nil
-        @children = []
-    end
-
-    def parent=(passed_node)
-        #delete the relationship between parent and child if parent isn't nil
-        self.parent.children.delete(self) if @parent != nil
-
-        @parent = passed_node
-
-        #if parent exists and if the child isn't already in the children array, then add yourself into the children
-        if !@parent.nil? && !@parent.children.include?(self)
-            @parent.children << self
+    attr_reader :value, :parent, :children
+        def initialize(value)
+            @value = value
+            @parent = nil
+            @children = []
         end
-    end
-
     
-  
-    def add_child(child)
-        child.parent = self
-    end
-
-    def remove_child(child) 
-        if child && self.children.include?(child)
-            child.parent = nil
-        else 
-            raise 'error'
+        def parent=(passed_node)
+            #delete the relationship between parent and child if parent isn't nil
+            self.parent.children.delete(self) if @parent != nil
+    
+            @parent = passed_node
+    
+            #if parent exists and if the child isn't already in the children array, then add yourself into the children
+            if !@parent.nil? && !@parent.children.include?(self)
+                @parent.children << self
+            end
         end
-    end
-
-    def dfs(target_value)
-        return self if self == target_value
-
-            
-    end
-end
-
+      
+        def add_child(child)
+            child.parent = self
+        end
     
-end
+        def remove_child(child) 
+            if child && self.children.include?(child)
+                child.parent = nil
+            else 
+                raise 'error'
+            end
+        end
+    
+        def dfs(target_value)
+            return self if @value == target_value
+             @children.each do |child| #b
+                result = child.dfs(target_value)
+                return result if result != nil
+            end
+            nil
+        end
+        
+    end
+    
+    
