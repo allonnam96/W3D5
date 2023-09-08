959b43012d0e4c76308b6e80b4b8f19963dfcab6
Harrison Liang <liangharrison123@gmail.com>
2023-09-08 14:20:05 -0400

    do parent=

--- PolyTreeNode/lib/tree_node.rb
+++ PolyTreeNode/lib/tree_node.rb
@@ -6,6 +6,18 @@ attr_reader :value, :parent, :children
         @parent = nil
         @children = []
     end
+
+    def parent=(passed_node)
+        #delete the relationship between parent and child if parent isn't nil
+        self.parent.children.delete(self) if @parent != nil
+
+        @parent = passed_node
+
+        #if parent exists and if the child isn't already in the children array, then add yourself into the children
+        if !@parent.nil? && !@parent.children.include?(self)
+            @parent.children << self
+        end
+    end
   
     
 end
